1048461265a71c32a7293f63cfe2ef98d4253aaa
MingaKoala <bened@DESKTOP-L4E7VTJ.localdomain>
2022-02-08 15:05:16 +0100

    models

---
+++ polls/models.py
@@ -0,0 +1,12 @@
+from unicodedata import name
+from django.db import models
+
+class Poll(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField()
+
+class Choice(models.Model):
+    poll = models.ForeignKey(to='Poll', on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    votes = models.IntegerField(default=0)
+    notes = models.TextField(null=True, blank=True)
