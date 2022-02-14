a37f707468038e01e61c94291ee27dac2e6a3735
MingaKoala <bened@DESKTOP-L4E7VTJ.localdomain>
2022-02-14 15:12:48 +0100

    HttpResponse problem solved

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,14 @@
+from random import choice
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import Poll
+
+
+
+def index(request):
+    choice = ""
+    for poll in Poll.objects.all():
+        choice = choice + " " + poll.name
+    return HttpResponse(choice) # Test
