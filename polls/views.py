382ee764658c498c7a5aa3a80e32ce10c8438af8
MingaKoala <bened@DESKTOP-L4E7VTJ.localdomain>
2022-02-16 13:18:47 +0100

    Session

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from multiprocessing import context
+from pyexpat.errors import messages
 from random import choice
 from urllib import request
 from django.http import HttpResponse, HttpResponseRedirect
@@ -9,43 +10,61 @@ from django.views import generic
 
 from .models import Poll, Choice
 
-
-
 def index(request):
-    context = { 'umfragen': Poll.objects.all(), 'titel': "Umfrageseite"}
+    context = {'umfragen': Poll.objects.all(), 'titel': "Umfragenseite"}
     return render(request=request, template_name='polls/index.html',
-                    context=context)
-
+                  context=context)
 
 class PollDetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/umfrage.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        user_can_see_results = False
+        if 'voted_polls' in self.request.session:
+            if type(self.request.session['voted_polls']) == list:
+                if context['object'].id in self.request.session['voted_polls']:
+                    user_can_see_results = True
+        context['voted'] = user_can_see_results
+
+        return context
+
 class ResultsDetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/results.html'
 
- 
-def umfrage_detail(request, slug):
-    umfrage= get_object_or_404(Poll, slug=slug)
-    context = {'umfrage': umfrage}
-    return render(request=request, template_name='polls/umfrage.html',
-                    context=context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-def results(request, slug):
-    umfrage= get_object_or_404(Poll, slug=slug)
-    context = {'umfrage': umfrage}
-    return render(request=request, template_name='polls/results.html',
-                    context=context)
+        user_can_see_results = False
+        if 'voted_polls' in self.request.session:
+            if type(self.request.session['voted_polls']) == list:
+                if context['object'].id in self.request.session['voted_polls']:
+                    user_can_see_results = True
+        context['access'] = user_can_see_results
+
+        return context
 
 def vote(request, slug):
-    umfrage= get_object_or_404(Poll, slug=slug)
+    umfrage = get_object_or_404(Poll, slug=slug)
     try:
         ausgewahlte_antwort = umfrage.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return HttpResponse("Fehler: es wurde keine bzw. eine ungültige Antwort ausgewählt!")
+        messages.error(request, "Fehler: keine bzw. eine ungueltige Antwort ausgewaehlt!")
+        return HttpResponseRedirect(reverse('polls:umfrage-detail', args=(umfrage.slug,)))
     else:
         ausgewahlte_antwort.votes += 1
         ausgewahlte_antwort.save()
+
+        if 'voted_polls' in request.session:
+            if type(request.session['voted_polls']) == list:
+                request.session['voted_polls'].append(umfrage.id)
+            else:
+                request.session['voted_polls'] = [umfrage.id]
+        else:
+            request.session['voted_polls'] = [umfrage.id]
+
         return HttpResponseRedirect(reverse('polls:results', args=(umfrage.slug,)))
 
