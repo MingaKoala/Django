8da25b2d3c2a1e8c7c47f5e5b98f66c36ffacc6e
MingaKoala <bened@DESKTOP-L4E7VTJ.localdomain>
2022-02-18 14:49:27 +0100

    Time

--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,9 @@ class Poll(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(unique=True)
 
+    publish_time =models.DateTimeField()
+    days_running = models.IntegerField(default=7)
+
     def __str__(self):
         return "{0} (Slug: {1})".format(self.name, self.slug)
 
